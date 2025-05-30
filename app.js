0c708b6fd5319362b1e8060de0aaaa7244add02e
Neftalem <dneftalem@gmail.com>
2025-05-30 23:00:33 +0300

    add js code add for class add querySelector & add APIkey

---
+++ app.js
@@ -0,0 +1,6 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+
+const apiKey = "904e30c385e3bd964b8b0072df049344";
+

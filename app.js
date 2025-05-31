82cf32fb00abb725487863794f1055a1a7ae1034
Neftalem <dneftalem@gmail.com>
2025-05-31 09:20:58 +0300

    add the weatherId & weather emoji

--- app.js
+++ app.js
@@ -65,7 +65,23 @@ function displayInfo(data) {
 
 function getWeatherEmoji(weatherId) {
 
-
+       if (weatherId >= 200 && weatherId < 300) {
+        return "⛈️";
+    } else if (weatherId >= 300 && weatherId < 500) {
+        return "🌦️";
+    } else if (weatherId >= 500 && weatherId < 600) {
+        return "🌧️";
+    } else if (weatherId >= 600 && weatherId < 700) {
+        return "❄️";
+    } else if (weatherId >= 700 && weatherId < 800) {
+        return "🌫️";
+    } else if (weatherId === 800) {
+        return "☀️";
+    } else if (weatherId > 800) {
+        return "☁️";
+    } else {
+        return "❓";
+    }
 
 };
 
